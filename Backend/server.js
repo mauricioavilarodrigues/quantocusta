7f4461b01d9a57610edb3e22d0ca4848861571b2
Brenda <bere.nda26@gmail.com>
2026-01-28 20:08:06 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,21 @@
 // init
 
+import express from "express";
+import cors from "cors";
+import routes from "./routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.send("Quantocusta API rodando 🚀");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando em http://localhost:" + PORT);
+});
