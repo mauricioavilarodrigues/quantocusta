d9bb250de910d1374b1863883f37ce554872c309
Maurício Avila <mauricioavilarodrigues@gmail.com>
2026-01-31 12:22:41 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -46,3 +46,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando em http://localhost:" + PORT);
 });
+
+let contador = 0;
+
+app.post("/api/clique", (req, res) => {
+  contador++;
+  res.json({ total: contador });
+});
