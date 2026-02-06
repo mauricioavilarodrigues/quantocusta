becc752c9a8334ffa2f448c075d3218ec98e4157
Maurício Avila <mauricioavilarodrigues@gmail.com>
2026-02-06 10:11:13 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,10 +3,9 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = "https://SEU_PROJETO.supabase.co";
 const supabaseKey = "SUA_PUBLIC_ANON_KEY";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Login com Google
-export async function loginComGoogle() {
+export async function loginWithGoogle() {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google"
   });
@@ -14,16 +13,4 @@ export async function loginComGoogle() {
   if (error) {
     alert("Erro no login: " + error.message);
   }
-}
-
-// Logout
-export async function logout() {
-  await supabase.auth.signOut();
-}
-
-// Monitorar sessão
-export function onAuthChange(callback) {
-  supabase.auth.onAuthStateChange((event, session) => {
-    callback(session);
-  });
 }
